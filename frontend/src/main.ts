ca5f0d76edc41d27c0928e45654e431a02c0d4b7
xerxes <a1182755839@gamil.com>
2026-05-25 13:52:24 +0800

    fix: use chinese element plus locale

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,10 @@
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 import { createApp } from "vue";
 
 import App from "./App.vue";
 import router from "./router";
 import "./styles/main.css";
 
-createApp(App).use(router).use(ElementPlus).mount("#app");
+createApp(App).use(router).use(ElementPlus, { locale: zhCn }).mount("#app");
